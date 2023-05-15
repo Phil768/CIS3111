d3deac80995d36799692d99a44989c922938e952
Phil768 <philippaul-2002@outlook.com>
2023-05-15 14:19:49 +0200

    .

--- cis3111-front-end/src/index.jsx
+++ cis3111-front-end/src/index.jsx
@@ -45,10 +45,10 @@ function Container() {
         "https://api-dot-cis3111-2023-assignment-1.ew.r.appspot.com/storeNumbers";
       //Starting message.
       console.log(">>!Started!<<");
-      for (let i = 0; i < 10; i++) {
+      for (let i = 0; i < 1000; i++) {
         //Creating a new array witch each iteration to hold teh current batch.
         const batch = [];
-        for (let j = 0; j < 1000; j++) {
+        for (let j = 0; j < 10; j++) {
           //Generating a random number between 0 and 100,000.
           const randomNumber = Math.floor(Math.random() * 100001);
           //Pushing the number to an array.
